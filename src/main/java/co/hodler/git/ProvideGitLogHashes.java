00c931f0ee4d9c353ac85447a15cc6e25ff5489a
xorrr <derxor@gmail.com>
2015-11-01 14:44:16 +0100

    implement fetching the commit log

---
+++ src/main/java/co/hodler/git/ProvideGitLogHashes.java
@@ -0,0 +1,34 @@
+package co.hodler.git;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.revwalk.RevCommit;
+
+import co.hodler.ProvideLog;
+import co.hodler.model.CommitHash;
+
+public class ProvideGitLogHashes implements ProvideLog {
+
+  private Git git;
+
+  public ProvideGitLogHashes(Git git) {
+    this.git = git;
+  }
+
+  @Override
+  public Set<CommitHash> provide() {
+    try {
+      Iterable<RevCommit> commits = git.log().all().call();
+      Set<CommitHash> gitLogHashes = new LinkedHashSet<>();
+      for (RevCommit commit : commits) {
+        gitLogHashes.add(new CommitHash(commit.getId().name()));
+      }
+      return gitLogHashes;
+    } catch (Exception e) {
+      throw new RuntimeException("Could not get the git log", e);
+    }
+  }
+
+}
