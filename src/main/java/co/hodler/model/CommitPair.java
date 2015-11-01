0ef276b80f9f092b5c828a95fb92a42c0b56f7ad
xorrr <derxor@gmail.com>
2015-11-01 11:53:23 +0100

    hashCode and equals for CommitPair

--- src/main/java/co/hodler/model/CommitPair.java
+++ src/main/java/co/hodler/model/CommitPair.java
@@ -18,4 +18,37 @@ public class CommitPair {
     return lowerCommitHash;
   }
 
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result
+        + ((lowerCommitHash == null) ? 0 : lowerCommitHash.hashCode());
+    result = prime * result
+        + ((upperCommitHash == null) ? 0 : upperCommitHash.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    CommitPair other = (CommitPair) obj;
+    if (lowerCommitHash == null) {
+      if (other.lowerCommitHash != null)
+        return false;
+    } else if (!lowerCommitHash.equals(other.lowerCommitHash))
+      return false;
+    if (upperCommitHash == null) {
+      if (other.upperCommitHash != null)
+        return false;
+    } else if (!upperCommitHash.equals(other.upperCommitHash))
+      return false;
+    return true;
+  }
+
 }
