6a57747a1a964b5a9c39d06ac9f5d24eaaec9a55
xorrr <derxor@gmail.com>
2015-11-03 21:42:39 +0100

    integration test git log

--- src/it/java/co/hodler/ProvideGitLogIT.java
+++ src/it/java/co/hodler/ProvideGitLogIT.java
@@ -1,13 +1,30 @@
 package co.hodler;
 
-import static org.junit.Assert.assertNotNull;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
+import java.io.File;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.eclipse.jgit.api.Git;
 import org.junit.Test;
+
+import co.hodler.git.ProvideGitLogHashes;
+import co.hodler.model.CommitHash;
 
 public class ProvideGitLogIT {
 
   @Test
-  public void beAbleToReadRepository() {
-    assertNotNull(getClass().getResource("test.txt").getPath());
+  public void canReadCommits() throws Exception {
+    File gitWorkDir = new File(System.getenv("REPO_DIR").toString());
+    Git git = Git.open(gitWorkDir);
+
+    ProvideGitLogHashes provideGitLogHashes = new ProvideGitLogHashes(git);
+    Set<CommitHash> hashes = provideGitLogHashes.provide();
+
+    Iterator<CommitHash> commitLogIterator = hashes.iterator();
+    assertThat(commitLogIterator.next(), is(new CommitHash("42cee89dc8e12feda94b1f1aa84d6c57ba8f633a")));
+    assertThat(commitLogIterator.next(), is(new CommitHash("358808125d4f9292dcc105e264177e6837bf8809")));
   }
 }
