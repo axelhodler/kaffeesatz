a63d379d32b609091823a42849c1117860b7036c
xorrr <derxor@gmail.com>
2015-11-07 22:22:53 +0100

    interface for progressbardisplay

---
+++ src/main/java/co/hodler/kaffeesatz/DisplayProgressBar.java
@@ -0,0 +1,27 @@
+package co.hodler.kaffeesatz;
+
+public interface DisplayProgressBar {
+
+  void begin();
+
+  void full();
+
+  void tenPercentDone();
+
+  void twentyPercentDone();
+
+  void thrityPercentDone();
+
+  void fourtyPercentDone();
+
+  void fiftyPercentDone();
+
+  void sixtyPercentDone();
+
+  void seventyPercentDone();
+
+  void eightyPercentDone();
+
+  void ninetyPercentDone();
+
+}
