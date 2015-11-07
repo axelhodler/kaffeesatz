24f72e06584be7ac8b056bd07076cf8e7d105e74
xorrr <derxor@gmail.com>
2015-11-07 19:08:13 +0100

    refactor test readability

--- src/test/java/co/hodler/kaffeesatz/actions/git/GitFetchChangedFilesShould.java
+++ src/test/java/co/hodler/kaffeesatz/actions/git/GitFetchChangedFilesShould.java
@@ -8,6 +8,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -28,22 +29,38 @@ public class GitFetchChangedFilesShould {
   @Mock
   ProvideChangesBetweenTwoCommits changesForPairProvider;
 
+  private FetchChangedFiles changedFiles;
+
+  @Before
+  public void setUp() {
+    changedFiles = new GitFetchChangedFiles(commitPairProvider,
+        changesForPairProvider);
+  }
+
   @Test
   public void provideAllChangedFiles() {
-    LinkedCommitHashPair pair = new LinkedCommitHashPair(new CommitHash("ef3cc2dc4c04be7c87af619ad1d7cc1d6f11bb26"),
-        new CommitHash("63c29e3323b5cec97a6214a22110fa8d7dae660b"));
-    Set<LinkedCommitHashPair> pairs = new HashSet<>();
-    pairs.add(pair);
-    given(commitPairProvider.providePairs()).willReturn(pairs);
-    Set<String> changes = new HashSet<>();
-    changes.add(".gitignore");
-    changes.add("/src/main/java/App.java");
-    given(changesForPairProvider.fetchChangesBetween(pair)).willReturn(changes);
-    FetchChangedFiles changedFiles = new GitFetchChangedFiles(commitPairProvider, changesForPairProvider);
+    willHaveTwoChangedFiles(oneCommitPair());
 
     List<String> filesChanged = changedFiles.fetchChangedFiles();
 
     assertThat(filesChanged, hasItem(".gitignore"));
     assertThat(filesChanged, hasItem("/src/main/java/App.java"));
+  }
+
+  private void willHaveTwoChangedFiles(LinkedCommitHashPair pair) {
+    Set<String> changes = new HashSet<>();
+    changes.add(".gitignore");
+    changes.add("/src/main/java/App.java");
+    given(changesForPairProvider.fetchChangesBetween(pair)).willReturn(changes);
+  }
+
+  private LinkedCommitHashPair oneCommitPair() {
+    LinkedCommitHashPair pair = new LinkedCommitHashPair(
+        new CommitHash("ef3cc2dc4c04be7c87af619ad1d7cc1d6f11bb26"),
+        new CommitHash("63c29e3323b5cec97a6214a22110fa8d7dae660b"));
+    Set<LinkedCommitHashPair> pairs = new HashSet<>();
+    pairs.add(pair);
+    given(commitPairProvider.providePairs()).willReturn(pairs);
+    return pair;
   }
 }
