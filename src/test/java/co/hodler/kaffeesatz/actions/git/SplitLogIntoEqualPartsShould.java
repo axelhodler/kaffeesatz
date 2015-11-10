d60d669b3bac3e8e88407c5272513c61551775bc
xorrr <derxor@gmail.com>
2015-11-10 21:51:45 +0100

    refactor, improve readability

--- src/test/java/co/hodler/kaffeesatz/actions/git/SplitLogIntoEqualPartsShould.java
+++ src/test/java/co/hodler/kaffeesatz/actions/git/SplitLogIntoEqualPartsShould.java
@@ -8,6 +8,7 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.hamcrest.Matcher;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -41,10 +42,10 @@ public class SplitLogIntoEqualPartsShould {
 
     List<Set<CommitHash>> logParts = splitLog.splitInto(2);
 
-    assertThat(logParts.get(0), hasItem(new CommitHash("2e121305db8fa36f1dbc6083e628b245e20ef4c4")));
-    assertThat(logParts.get(0), hasItem(new CommitHash("5633c849c06f6063fdca5dec5054950e403447b8")));
-    assertThat(logParts.get(1), hasItem(new CommitHash("5633c849c06f6063fdca5dec5054950e403447b8")));
-    assertThat(logParts.get(1), hasItem(new CommitHash("1b937db69b4f7edb519b051ef8319de56de6f627")));
+    assertThat(first(logParts), hasCommitHash("2e121305db8fa36f1dbc6083e628b245e20ef4c4"));
+    assertThat(first(logParts), hasCommitHash("5633c849c06f6063fdca5dec5054950e403447b8"));
+    assertThat(second(logParts), hasCommitHash("5633c849c06f6063fdca5dec5054950e403447b8"));
+    assertThat(second(logParts), hasCommitHash("1b937db69b4f7edb519b051ef8319de56de6f627"));
   }
 
   @Test
@@ -53,12 +54,12 @@ public class SplitLogIntoEqualPartsShould {
 
     List<Set<CommitHash>> logParts = splitLog.splitInto(3);
 
-    assertThat(logParts.get(0), hasItem(new CommitHash("1e121305db8fa36f1dbc6083e628b245e20ef4c1")));
-    assertThat(logParts.get(0), hasItem(new CommitHash("2633c849c06f6063fdca5dec5054950e403447b2")));
-    assertThat(logParts.get(1), hasItem(new CommitHash("2633c849c06f6063fdca5dec5054950e403447b2")));
-    assertThat(logParts.get(1), hasItem(new CommitHash("3633c849c06f6063fdca5dec5054950e403447b3")));
-    assertThat(logParts.get(2), hasItem(new CommitHash("3633c849c06f6063fdca5dec5054950e403447b3")));
-    assertThat(logParts.get(2), hasItem(new CommitHash("4633c849c06f6063fdca5dec5054950e403447b4")));
+    assertThat(first(logParts), hasCommitHash("1e121305db8fa36f1dbc6083e628b245e20ef4c1"));
+    assertThat(first(logParts), hasCommitHash("2633c849c06f6063fdca5dec5054950e403447b2"));
+    assertThat(second(logParts), hasCommitHash("2633c849c06f6063fdca5dec5054950e403447b2"));
+    assertThat(second(logParts), hasCommitHash("3633c849c06f6063fdca5dec5054950e403447b3"));
+    assertThat(third(logParts), hasCommitHash("3633c849c06f6063fdca5dec5054950e403447b3"));
+    assertThat(third(logParts), hasCommitHash("4633c849c06f6063fdca5dec5054950e403447b4"));
   }
 
   @Test
@@ -67,11 +68,11 @@ public class SplitLogIntoEqualPartsShould {
 
     List<Set<CommitHash>> logParts = splitLog.splitInto(2);
 
-    assertThat(logParts.get(0), hasItem(new CommitHash("1e121305db8fa36f1dbc6083e628b245e20ef4c1")));
-    assertThat(logParts.get(0), hasItem(new CommitHash("2633c849c06f6063fdca5dec5054950e403447b2")));
-    assertThat(logParts.get(0), hasItem(new CommitHash("3633c849c06f6063fdca5dec5054950e403447b3")));
-    assertThat(logParts.get(1), hasItem(new CommitHash("3633c849c06f6063fdca5dec5054950e403447b3")));
-    assertThat(logParts.get(1), hasItem(new CommitHash("4633c849c06f6063fdca5dec5054950e403447b4")));
+    assertThat(first(logParts), hasCommitHash("1e121305db8fa36f1dbc6083e628b245e20ef4c1"));
+    assertThat(first(logParts), hasCommitHash("2633c849c06f6063fdca5dec5054950e403447b2"));
+    assertThat(first(logParts), hasCommitHash("3633c849c06f6063fdca5dec5054950e403447b3"));
+    assertThat(second(logParts), hasCommitHash("3633c849c06f6063fdca5dec5054950e403447b3"));
+    assertThat(second(logParts), hasCommitHash("4633c849c06f6063fdca5dec5054950e403447b4"));
   }
 
   @Test
@@ -80,14 +81,14 @@ public class SplitLogIntoEqualPartsShould {
 
     List<Set<CommitHash>> logParts = splitLog.splitInto(4);
 
-    assertThat(logParts.get(0), hasItem(new CommitHash("1e121305db8fa36f1dbc6083e628b245e20ef4c1")));
-    assertThat(logParts.get(0), hasItem(new CommitHash("2633c849c06f6063fdca5dec5054950e403447b2")));
-    assertThat(logParts.get(1), hasItem(new CommitHash("2633c849c06f6063fdca5dec5054950e403447b2")));
-    assertThat(logParts.get(1), hasItem(new CommitHash("3633c849c06f6063fdca5dec5054950e403447b3")));
-    assertThat(logParts.get(2), hasItem(new CommitHash("3633c849c06f6063fdca5dec5054950e403447b3")));
-    assertThat(logParts.get(2), hasItem(new CommitHash("4633c849c06f6063fdca5dec5054950e403447b4")));
-    assertThat(logParts.get(3), hasItem(new CommitHash("4633c849c06f6063fdca5dec5054950e403447b4")));
-    assertThat(logParts.get(3), hasItem(new CommitHash("5633c849c06f6063fdca5dec5054950e403447b4")));
+    assertThat(first(logParts), hasCommitHash("1e121305db8fa36f1dbc6083e628b245e20ef4c1"));
+    assertThat(first(logParts), hasCommitHash("2633c849c06f6063fdca5dec5054950e403447b2"));
+    assertThat(second(logParts), hasCommitHash("2633c849c06f6063fdca5dec5054950e403447b2"));
+    assertThat(second(logParts), hasCommitHash("3633c849c06f6063fdca5dec5054950e403447b3"));
+    assertThat(third(logParts), hasCommitHash("3633c849c06f6063fdca5dec5054950e403447b3"));
+    assertThat(third(logParts), hasCommitHash("4633c849c06f6063fdca5dec5054950e403447b4"));
+    assertThat(fourth(logParts), hasCommitHash("4633c849c06f6063fdca5dec5054950e403447b4"));
+    assertThat(fourth(logParts), hasCommitHash("5633c849c06f6063fdca5dec5054950e403447b4"));
   }
 
   private Set<CommitHash> fourCommits() {
@@ -104,5 +105,25 @@ public class SplitLogIntoEqualPartsShould {
     commitLogHashes.addAll(fourCommits());
     commitLogHashes.add(new CommitHash("5633c849c06f6063fdca5dec5054950e403447b4"));
     return commitLogHashes;
+  }
+
+  private Matcher<Iterable<? super CommitHash>> hasCommitHash(String commitHashValue) {
+    return hasItem(new CommitHash(commitHashValue));
+  }
+
+  private Set<CommitHash> fourth(List<Set<CommitHash>> logParts) {
+    return logParts.get(3);
+  }
+
+  private Set<CommitHash> third(List<Set<CommitHash>> logParts) {
+    return logParts.get(2);
+  }
+
+  private Set<CommitHash> second(List<Set<CommitHash>> logParts) {
+    return logParts.get(1);
+  }
+
+  private Set<CommitHash> first(List<Set<CommitHash>> logParts) {
+    return logParts.get(0);
   }
 }
