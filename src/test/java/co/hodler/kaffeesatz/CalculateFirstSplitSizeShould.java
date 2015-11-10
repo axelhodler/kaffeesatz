fefd0b0ad029e2f61a6914a39480d4129a86abad
xorrr <derxor@gmail.com>
2015-11-10 22:25:40 +0100

    narrow down logic

--- src/test/java/co/hodler/kaffeesatz/CalculateFirstSplitSizeShould.java
+++ src/test/java/co/hodler/kaffeesatz/CalculateFirstSplitSizeShould.java
@@ -3,21 +3,30 @@ package co.hodler.kaffeesatz;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculateFirstSplitSizeShould {
 
+  private CalculateFirstSplitSize calcFirstSplitSize;
+
+  @Before
+  public void setUp() {
+    calcFirstSplitSize = new CalculateFirstSplitSize();
+  }
+
   @Test
   public void beTwoWhenTwoPartsOfThreeCommitsIsDesired() {
-    CalculateFirstSplitSize calcFirstSplitSize = new CalculateFirstSplitSize();
-
     assertThat(calcFirstSplitSize.using(2, 3), is(2));
   }
 
   @Test
   public void beThreeWhenTwoPartsOfFourCommitsIsDesired() {
-    CalculateFirstSplitSize calcFirstSplitSize = new CalculateFirstSplitSize();
-
     assertThat(calcFirstSplitSize.using(2, 4), is(3));
+  }
+
+  @Test
+  public void beTwoWhenFourPartsOfFiveCommitsAreDesired() {
+    assertThat(calcFirstSplitSize.using(4, 5), is(2));
   }
 }
