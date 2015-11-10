a1dc0f1aea6c0525e591ce74573d1392264741bc
xorrr <derxor@gmail.com>
2015-11-10 22:23:17 +0100

    begin triangulation

--- src/main/java/co/hodler/kaffeesatz/CalculateFirstSplitSize.java
+++ src/main/java/co/hodler/kaffeesatz/CalculateFirstSplitSize.java
@@ -3,7 +3,12 @@ package co.hodler.kaffeesatz;
 public class CalculateFirstSplitSize {
 
   public int using(int parts, int amountOfCommits) {
-    return 2;
+    int firstPartSize = 0;
+    if (amountOfCommits == 4)
+      firstPartSize = 3;
+    else
+      firstPartSize = 2;
+    return firstPartSize;
   }
 
 }
--- src/test/java/co/hodler/kaffeesatz/CalculateFirstSplitSizeShould.java
+++ src/test/java/co/hodler/kaffeesatz/CalculateFirstSplitSizeShould.java
@@ -1,5 +1,8 @@
 package co.hodler.kaffeesatz;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 import org.junit.Test;
 
 public class CalculateFirstSplitSizeShould {
@@ -8,6 +11,13 @@ public class CalculateFirstSplitSizeShould {
   public void beTwoWhenTwoPartsOfThreeCommitsIsDesired() {
     CalculateFirstSplitSize calcFirstSplitSize = new CalculateFirstSplitSize();
 
-    calcFirstSplitSize.using(2, 3);
+    assertThat(calcFirstSplitSize.using(2, 3), is(2));
+  }
+
+  @Test
+  public void beThreeWhenTwoPartsOfFourCommitsIsDesired() {
+    CalculateFirstSplitSize calcFirstSplitSize = new CalculateFirstSplitSize();
+
+    assertThat(calcFirstSplitSize.using(2, 4), is(3));
   }
 }
