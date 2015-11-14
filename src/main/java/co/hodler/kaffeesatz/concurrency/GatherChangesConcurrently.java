204769427bcb22c45e64de64deed31bc87a28e89
xorrr <derxor@gmail.com>
2015-11-14 18:42:40 +0100

    initial class to gather changes concurrently

---
+++ src/main/java/co/hodler/kaffeesatz/concurrency/GatherChangesConcurrently.java
@@ -0,0 +1,34 @@
+package co.hodler.kaffeesatz.concurrency;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import co.hodler.kaffeesatz.actions.ProvideChangesBetweenTwoCommits;
+import co.hodler.kaffeesatz.model.LinkedCommitHashPair;
+import co.hodler.kaffeesatz.ui.TrackProgress;
+
+public class GatherChangesConcurrently {
+
+  private GatherChangesThreadFactory gatherChangesThreadFactory;
+  private GatherChangesFactory gatherChangesFactory;
+  private TrackProgress trackProgress;
+  private ProvideChangesBetweenTwoCommits provideChangesBetweenTwoCommits;
+
+  public GatherChangesConcurrently(
+      ProvideChangesBetweenTwoCommits provideChangesBetweenTwoCommits,
+      GatherChangesThreadFactory gatherChangesThreadFactory,
+      GatherChangesFactory gatherChangesFactory, TrackProgress trackProgress) {
+    this.provideChangesBetweenTwoCommits = provideChangesBetweenTwoCommits;
+    this.gatherChangesThreadFactory = gatherChangesThreadFactory;
+    this.gatherChangesFactory = gatherChangesFactory;
+    this.trackProgress = trackProgress;
+  }
+
+  public void gather(List<Set<LinkedCommitHashPair>> groupsOfCommitPairs) {
+    gatherChangesFactory.createThreadToGatherChanges(new HashSet<>(),
+        provideChangesBetweenTwoCommits, trackProgress, new ArrayList<>());
+  }
+
+}
---
+++ src/test/java/co/hodler/kaffeesatz/concurrency/GatherChangesConcurrentlyShould.java
@@ -0,0 +1,46 @@
+package co.hodler.kaffeesatz.concurrency;
+
+import static org.mockito.Mockito.verify;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import co.hodler.kaffeesatz.actions.ProvideChangesBetweenTwoCommits;
+import co.hodler.kaffeesatz.model.LinkedCommitHashPair;
+import co.hodler.kaffeesatz.ui.TrackProgress;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GatherChangesConcurrentlyShould {
+
+  @Mock
+  TrackProgress trackProgress;
+  @Mock
+  ProvideChangesBetweenTwoCommits provideChangesBetweenTwoCommits;
+  @Mock
+  GatherChangesFactory gatherChangesFactory;
+  @Mock
+  GatherChangesThreadFactory gatherChangesThreadFactory;
+
+  private GatherChangesConcurrently gatherChangesConcurrently;
+
+  @Test
+  public void create_threads_to_gather_changes() {
+    gatherChangesConcurrently = new GatherChangesConcurrently(
+        provideChangesBetweenTwoCommits, gatherChangesThreadFactory,
+        gatherChangesFactory, trackProgress);
+    List<Set<LinkedCommitHashPair>> groupsOfCommitPairs = new ArrayList<>();
+    groupsOfCommitPairs.add(new HashSet<>());
+
+    gatherChangesConcurrently.gather(groupsOfCommitPairs);
+
+    verify(gatherChangesFactory).createThreadToGatherChanges(new HashSet<>(),
+        provideChangesBetweenTwoCommits, trackProgress, new ArrayList<>());
+  }
+}
